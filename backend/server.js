72e03277879e2127b19ad44e86b3995d599a26a3
Akhilesh <akhileshkumarlal7@gmail.com>
2025-10-25 01:01:27 +0530

    Updated Backend code

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,15 @@ import authRoutes from "./routes/authRoutes.js";
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://akhileshlal28.github.io", // GitHub Pages domain
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/notes", router);
